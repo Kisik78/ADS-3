944be09a02c223a7aee9136f6dde2043136f2bb8
Коньков Арсений <95272498+Kisik78@users.noreply.github.com>
2022-03-11 13:05:04 +0300

    Konkov

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,19 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  int a = 0, k = 0, res = size - 1;
+    int uni = k + (res - k) / 2;
+    if (true) {
+      for (int k = 0; k < uni; k++) {
+            if (arr[k] == value)
+                y++;
+      }
+      for (int res = size; res >= uni; res--) {
+            if (arr[res] == value)
+                a++;
+      }
+      return a;
+    } else {
+      return 0;
+      }
 }
